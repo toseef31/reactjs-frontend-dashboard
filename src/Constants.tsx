c8f1c7e45fe3b57e9d48698303fd5d20a331951e
Toseef <peekinternational@gmail.com>
2024-09-13 13:37:44 +0500

    Updated Backend

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -1,6 +1,9 @@
 const constants = {
-    BASE_URL: 'https://db.piscatoria.co.nz/api',
-    BASE_ASSET_URL: 'https://db.piscatoria.co.nz/public',
+    // BASE_URL: 'https://db.piscatoria.co.nz/api',
+    // BASE_ASSET_URL: 'https://db.piscatoria.co.nz/public',
+
+    BASE_URL: 'https://db.piscatoria.co.nz/backend/api',
+    BASE_ASSET_URL: 'https://db.piscatoria.co.nz/backend/public',
 
     // BASE_URL: 'http://localhost:8000/api',
     // BASE_ASSET_URL: 'http://localhost:8000',
